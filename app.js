16f2704f391ba37ce03245eaf5fed048553f7486
ColdBeanSprouts <39546777+ColdBeanSprouts@users.noreply.github.com>
2022-08-03 10:48:22 +0800

    Update app.js
    尝试修复2_改签到地址

--- app.js
+++ app.js
@@ -34,12 +34,12 @@ let cookieSJ = process.env["SJCOOKIE"];
 
 
 const sjUrl =
-    "https://www.4ksj.com/qiandao/";
+    "https://www.4ksj.com/plugin.php?id=k_misign:sign&mobile=2";
 const hao4kUrl =
-    "https://www.hao4k.cn/qiandao/";
+    "https://www.hao4k.cn/plugin.php?id=k_misign:sign&mobile=2";
 
 const userAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36";
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
 
 const headers = {
     cookie: cookie ?? "",
