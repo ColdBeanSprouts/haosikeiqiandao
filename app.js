c1f76847b4d24ed18db377f7cbb15043f165bec3
ColdBeanSprouts <39546777+ColdBeanSprouts@users.noreply.github.com>
2022-08-06 14:32:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const hao4kUrl =
     "https://www.hao4k.cn/qiandao/";
 
 const userAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36";
 
 const headers = {
     cookie: cookie ?? "",
